2fe9ba1412921bb1dbabe83cc578d698d42c61ff
dengs <ds@hanzhisoft.com>
2023-08-14 15:15:56 +0800

    [New] 扩展 style-vars 定义

---
+++ src/theme/style-vars.js
@@ -0,0 +1,25 @@
+export const vars = {
+  // Brand Color
+  "--color-primary": "#409eff",
+  // Functional Color
+  "--color-success": "#67c23a",
+  "--color-warning": "#e6a23c",
+  "--color-danger": "#f56c6c",
+  "--color-info": "#909399",
+  // Font Color
+  "--color-text-primary": "#303133",
+  "--color-text-regular": "#606266",
+  "--color-text-secondary": "#909399",
+  "--color-text-placeholder": "#c0c4cc",
+  // Border Color
+  "--border-color-base": "#dcdfe6",
+  "--border-color-light": "#e4e7ed",
+  "--border-color-lighter": "#ebeef5",
+  "--border-color-extra-light": "#f2f6fc",
+  // Background Color
+  "--background-color-base": "#f5f7fa",
+  "--background-color-white": "#ffffff",
+  "--background-color-black": "#000000",
+};
+
+export default vars;
